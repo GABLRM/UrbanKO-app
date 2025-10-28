9174d099340b2b377ea000b6dbac1e5dec625666
ALATORRE <audran.latorre@ynov.com>
2025-10-28 16:40:11 +0100

    feat(LB-38): create onboarding

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -16,15 +16,15 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.background,
-        width: '100%',
-    },
-    innerContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    width: "100%",
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
 });
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,45 @@
-import { Colors } from '@/constants/Colors';
-import { Flame, Swords } from 'lucide-react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from "@/constants/Colors";
+import { Flame, Swords } from "lucide-react-native";
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native";
 
-export default function Header() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.headerContainer}>
-                <Swords color={Colors.primary} size={60} />
-                <Flame color={Colors.primary} size={60} />
-            </View>
-            <Text style={styles.headerTitle}>Urban KO</Text>
-            <Text style={styles.headerSubtitle}>Trouve ton adversaire et met le KO</Text>
-        </View>
-    );
+type HeaderProps = {
+  style?: StyleProp<ViewStyle>;
+};
+
+export default function Header({ style }: HeaderProps) {
+  return (
+    <View style={[styles.container, style]}>
+      <View style={styles.headerContainer}>
+        <Swords color={Colors.primary} size={60} />
+        <Flame color={Colors.primary} size={60} />
+      </View>
+      <Text style={styles.headerTitle}>Urban KO</Text>
+      <Text style={styles.headerSubtitle}>
+        Trouve ton adversaire et met le KO
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headerContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    headerTitle: {
-        color: Colors.primary,
-        fontSize: 40,
-        marginTop: 5,
-        fontWeight: '800',
-    },
-    headerSubtitle: {
-        color: Colors.secondary,
-        fontSize: 17,
-        fontWeight: '600',
-        marginTop: 10,
-    },
+  container: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    color: Colors.primary,
+    fontSize: 40,
+    marginTop: 5,
+    fontWeight: "800",
+  },
+  headerSubtitle: {
+    color: Colors.secondary,
+    fontSize: 17,
+    fontWeight: "600",
+    marginTop: 10,
+  },
 });
