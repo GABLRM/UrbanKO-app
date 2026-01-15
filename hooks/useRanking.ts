4cc638832d6ad922efa23b753376db5a8767ac29
Gabriel Laroumanie <g.laroumanie@gmail.com>
2026-01-15 22:51:08 +0100

    fix: my profile rank
    # Conflicts:
    #	app/(app)/(tabs)/profile/[id].tsx

--- hooks/useRanking.ts
+++ hooks/useRanking.ts
@@ -37,3 +37,37 @@ export function useRanking(discipline?: Disciplines | null) {
         staleTime: 30 * 1000, // 30 secondes
     });
 }
+
+export function useMyRank() {
+    return useQuery({
+        queryKey: ['myRank'],
+        queryFn: async (): Promise<{
+            user: User;
+            rank: number;
+            totalUsers: number;
+            percentile: number;
+        }> => {
+            const baseUrl = process.env.EXPO_PUBLIC_API_URL;
+            const token = await SecureStore.getItemAsync('token');
+
+            const response = await fetch(`${baseUrl}/ranking/me`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(
+                    `Erreur lors de la récupération du rang: ${response.status}, ${errorText}`,
+                );
+            }
+
+            const data = await response.json();
+            return data;
+        },
+        staleTime: 30 * 1000, // 30 secondes
+    });
+}
